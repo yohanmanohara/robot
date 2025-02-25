f155993d109063656c021d128efe5755e9b5d85c
yohanmanohara <warushayohan80@gmail.com>
2025-02-26 04:20:43 +0530

    #

--- src/main.cpp
+++ src/main.cpp
@@ -4,35 +4,22 @@
 LoadCellHandler loadCellHandler;
 
 
-// Configuration settings
-const int stepDelay = 200;  // Delay for each step (in microseconds)
+const int stepDelay = 200;  
 
-// Calculate the number of steps based on the desired movement time
-const int stepsForward = (1 * 1000000) / stepDelay;  // Steps to move forward in 40 seconds
-const int stepsBackward = (20 * 1000000) / stepDelay; // Steps to move backward in 50 seconds
+const int stepsForward = (1 * 1000000) / stepDelay;  
+const int stepsBackward = (20 * 1000000) / stepDelay; 
 
 void setup() {
     motorSetup();
-    loadCellHandler.begin(); // Initialize motor pins
+    loadCellHandler.begin();
 }
-
 void loop() {
-    // Move forward for 40 seconds
-    // moveForward(stepsForward, stepDelay); // Adjust final delay as needed
-    // delay(1000); // Optional delay after moving forward
+    moveForward(stepsForward, stepDelay);
+    delay(1000); 
 
-    // // Move backward for 50 seconds
-    // moveBackward(stepsBackward, stepDelay); // Adjust final delay as needed
-    // delay(1000); // Optional delay after moving backward
-    // loadCellHandler.update();
+ 
 
-    // // Receive command from serial terminal, send 't' to initiate tare operation:
-    // if (Serial.available() > 0) {
-    //     char inByte = Serial.read();
-    //     if (inByte == 't') {
-    //         loadCellHandler.tare();
-    //     }
-    // }
+   
 }
 
 
